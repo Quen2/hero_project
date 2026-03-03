2e150367b833494b1fe07808079f361c306ec284
QuentinG <ZexFrost1@gmail.com>
2026-03-03 09:54:21 +0100

    [+] First push

---
+++ hero-project/src/components/Navigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <RouterLink to="/">Home</RouterLink>
+  <RouterLink to="/about">About</RouterLink>
+</template>
+
+<script>
+  export default {
+    name: 'Navigation',
+  }
+</script>
